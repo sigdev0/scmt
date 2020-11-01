dbe2de0d94c8f6cf268d06151dce0e6b0a826ac8
Akbar-SM <akbarsidikmaulana@gmail.com>
2020-11-02 00:48:32 +0700

    Added `business_unit_id` to PR

--- resources/api/purchase-requisition.js
+++ resources/api/purchase-requisition.js
@@ -11,7 +11,8 @@ GET('purchase-requisition/:id', () => {
         };
 
     validate(data, rule, () => {
-        var pr = PR.select('purchase_requisitions.id', 'number', 'remarks', 'status', 'processed_date', 'approved_date', 'cancelled_date', 'purchase_requisitions.created_at', 'purchase_requisitions.updated_at', 'processors.username as processed_by', 'approvers.username as approved_by', 'cancellers.username as cancelled_by', 'creators.username as created_by', 'updaters.username as updated_by')
+        var pr = PR.select('purchase_requisitions.id', 'number', 'remarks', 'status', 'processed_date', 'approved_date', 'cancelled_date', 'purchase_requisitions.created_at', 'purchase_requisitions.updated_at', 'locations.location_code as business_unit', 'processors.username as processed_by', 'approvers.username as approved_by', 'cancellers.username as cancelled_by', 'creators.username as created_by', 'updaters.username as updated_by')
+            .leftJoin('locations', 'locations.id', 'business_unit_id')
             .leftJoin('users as processors', 'processors.id', 'purchase_requisitions.processed_by')
             .leftJoin('users as approvers', 'approvers.id', 'purchase_requisitions.approved_by')
             .leftJoin('users as cancellers', 'cancellers.id', 'purchase_requisitions.cancelled_by')
@@ -34,7 +35,8 @@ GET('purchase-requisition/:id', () => {
 GET('purchase-requisition', () => {
     var pr = PR.instance();
 
-    pr.select('purchase_requisitions.id', 'number', 'remarks', 'status', 'processed_date', 'approved_date', 'cancelled_date', 'purchase_requisitions.created_at', 'purchase_requisitions.updated_at', 'processors.username as processed_by', 'approvers.username as approved_by', 'cancellers.username as cancelled_by', 'creators.username as created_by', 'updaters.username as updated_by')
+    pr.select('purchase_requisitions.id', 'number', 'remarks', 'status', 'processed_date', 'approved_date', 'cancelled_date', 'purchase_requisitions.created_at', 'purchase_requisitions.updated_at', 'locations.location_code as business_unit', 'processors.username as processed_by', 'approvers.username as approved_by', 'cancellers.username as cancelled_by', 'creators.username as created_by', 'updaters.username as updated_by')
+        .leftJoin('locations', 'locations.id', 'business_unit_id')
         .leftJoin('users as processors', 'processors.id', 'purchase_requisitions.processed_by')
         .leftJoin('users as approvers', 'approvers.id', 'purchase_requisitions.approved_by')
         .leftJoin('users as cancellers', 'cancellers.id', 'purchase_requisitions.cancelled_by')
@@ -69,25 +71,26 @@ GET('purchase-requisition', () => {
 /* PR List Datatable */
 GET('purchase-requisition-datatable', () => {
     var instance = PR.instance(),
-        columnToSelect = ['purchase_requisitions.id', 'number', 'remarks', 'status', 'processed_date', 'approved_date', 'cancelled_date', 'purchase_requisitions.created_at', 'purchase_requisitions.updated_at', 'processors.username as processed_by', 'approvers.username as approved_by', 'cancellers.username as cancelled_by', 'creators.username as created_by', 'updaters.username as updated_by'],
+        columnToSelect = ['purchase_requisitions.id', 'number', 'remarks', 'status', 'processed_date', 'approved_date', 'cancelled_date', 'purchase_requisitions.created_at', 'purchase_requisitions.updated_at', 'business_unit', 'processors.username as processed_by', 'approvers.username as approved_by', 'cancellers.username as cancelled_by', 'creators.username as created_by', 'updaters.username as updated_by'],
         columnToSearch = ['purchase_requisitions.number', 'purchase_requisitions.remarks'];
 
-    instance.leftJoin('users as processors', 'processors.id', 'purchase_requisitions.processed_by')
-        .leftJoin('users as approvers', 'approvers.id', 'purchase_requisitions.approved_by')
-        .leftJoin('users as cancellers', 'cancellers.id', 'purchase_requisitions.cancelled_by')
-        .leftJoin('users as creators', 'creators.id', 'purchase_requisitions.created_by')
-        .leftJoin('users as updaters', 'updaters.id', 'purchase_requisitions.updated_by');
+    instance.leftJoin('locations', 'locations.id', 'business_unit_id')
+            .leftJoin('users as processors', 'processors.id', 'purchase_requisitions.processed_by')
+            .leftJoin('users as approvers', 'approvers.id', 'purchase_requisitions.approved_by')
+            .leftJoin('users as cancellers', 'cancellers.id', 'purchase_requisitions.cancelled_by')
+            .leftJoin('users as creators', 'creators.id', 'purchase_requisitions.created_by')
+            .leftJoin('users as updaters', 'updaters.id', 'purchase_requisitions.updated_by');
 
     res(instance.datatable(columnToSelect, columnToSearch));
 });
 
 /* PR Insert */
 POST('purchase-requisition/insert', () => {
-    var data = req('number', 'remarks', 'status', 'created_by', 'status'),
+    var data = req('number', 'remarks', 'status', 'created_by', 'status', 'business_unit_id'),
         rule = {
-            number: ['required', 'unique:purchase_requisitions'],
-            status : ['required', 'in:active,pending,cancel'],
-            created_by: ['required', 'exists:users,id']
+            number      : ['required', 'unique:purchase_requisitions'],
+            status      : ['required', 'in:active,pending,cancel'],
+            created_by  : ['required', 'exists:users,id']
         };
 
     validate(data, rule, () => {
@@ -101,7 +104,7 @@ POST('purchase-requisition/insert', () => {
         var purchase_requisition = PR.insert(data);
 
         if (purchase_requisition) {
-            console.log(req('details'));
+            // console.log(req('details'));
             for (var i = 0; i < count(req('details')); i++) {
                 var detail = PRD.instance();
                 detail.id = PRD.max('id') + 1;
@@ -125,11 +128,11 @@ POST('purchase-requisition/insert', () => {
 
 /* PR Update */
 PUT('purchase-requisition/update', () => {
-    var data = req('id', 'number', 'remarks', 'status', 'updated_by'),
+    var data = req('id', 'number', 'remarks', 'status', 'updated_by', 'business_unit_id'),
         rule = {
-            id: ['required', 'exists:purchase_requisitions'],
-            number: ['required', 'unqiue:purchase_requisitions,number,id,' + data.number],
-            updated_by: ['required', 'exists:users,id']
+            id          : ['required', 'exists:purchase_requisitions'],
+            number      : ['required', 'unqiue:purchase_requisitions,number,id,' + data.number],
+            updated_by  : ['required', 'exists:users,id']
         };
 
     validate(data, rule, () => {
@@ -190,36 +193,40 @@ DELETE('purchase-requisition/delete/:id', () => {
 PUT('purchase-requisition/set-status/:action/:id', () => {
     var data = Object.assign(param(), req().all()),
         rule = {
-            id: ['required', 'exists:purchase_requisitions'],
-            action: ['required', 'in:approve,cancel,process'],
-            user: ['required', 'exists:users,id']
+            id      : ['required', 'exists:purchase_requisitions'],
+            action  : ['required', 'in:approve,cancel,process'],
+            user    : ['required', 'exists:users,id']
         };
 
     validate(data, rule, () => {
         var purchase_requisition = PR.find(data.id),
             action = '';
 
-        purchase_requisition.approved_date = null;
-        purchase_requisition.approved_by = null;
+        purchase_requisition.approved_date  = null;
+        purchase_requisition.approved_by    = null;
         purchase_requisition.cancelled_date = null;
-        purchase_requisition.cancelled_by = null;
+        purchase_requisition.cancelled_by   = null;
         purchase_requisition.processed_date = null;
-        purchase_requisition.processed_by = null;
+        purchase_requisition.processed_by   = null;
 
         if (data.action === 'approve') {
             action = 'approved';
+            purchase_requisition.status = 'approved';
             purchase_requisition.approved_date = now(true);
             purchase_requisition.approved_by = data.user;
         } else if (data.action === 'cancel') {
             action = 'cancelled';
+            purchase_requisition.status = 'cancelled';
             purchase_requisition.cancelled_date = now(true);
             purchase_requisition.cancelled_by = data.user;
         } else if (data.action === 'process') {
             action = 'processed';
+            purchase_requisition.status = 'processed';
             purchase_requisition.processed_date = now(true);
             purchase_requisition.processed_by = data.user;
         }
 
+        console.log(purchase_requisition.props());
         if (purchase_requisition.update()) {
             res(`Purchase Requisition with ID '${data.id}' successfully '${action}'`);
         } else {
